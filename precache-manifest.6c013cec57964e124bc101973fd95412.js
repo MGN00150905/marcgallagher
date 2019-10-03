dcc6212d43671dd61c8d0afeb3c9c882b7d751c3
Marc <marcgallagher16@gmail.com>
2019-10-03 23:03:06 +0100

    Updates

---
+++ precache-manifest.6c013cec57964e124bc101973fd95412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79125dc4dd88ccc74ac0",
+    "url": "/marcgallagher/static/js/runtime~main.79125dc4.js"
+  },
+  {
+    "revision": "71772b12c0f7dbfddfa3",
+    "url": "/marcgallagher/static/js/main.71772b12.chunk.js"
+  },
+  {
+    "revision": "e990ccfaedfecdf9af24",
+    "url": "/marcgallagher/static/js/1.e990ccfa.chunk.js"
+  },
+  {
+    "revision": "71772b12c0f7dbfddfa3",
+    "url": "/marcgallagher/static/css/main.ed6910c2.chunk.css"
+  },
+  {
+    "revision": "1218a6b94300bd9fbc8af7a755a2217e",
+    "url": "/marcgallagher/index.html"
+  }
+];
